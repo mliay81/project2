c419389f50ba460930a95bfa2506b94f59de1cdd
Sujatha <sujatha012@hotmail.com>
2018-08-23 05:08:56 -0500

    fixing conflicts

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-
+var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Handlebars
@@ -20,14 +22,27 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-// // Routes
+// Routes
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+var syncOptions = { force: false };
 
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
 
-
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
+
 module.exports = app;
