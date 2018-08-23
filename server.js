a436f116757efad355392d577c38f67cff459c0d
Sujatha <sujatha012@hotmail.com>
2018-08-23 04:52:57 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-require("dotenv").config();
 var express = require("express");
-var bodyParser = require("body-parser");
+
 var exphbs = require("express-handlebars");
 
-var db = require("./models");
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
 app.use(express.static("public"));
 
 // Handlebars
@@ -22,27 +20,14 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
+// // Routes
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+
 });
-
 module.exports = app;
