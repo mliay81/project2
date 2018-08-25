8aed0e72726d2062a401a5d5d40c5f563af89fb5
Abdibasid Osman <mistuyeta@me.com>
2018-08-25 11:30:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ var path = requeire('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'cheers_db',
+});
+
+connection.connect(function(err){
+  if(err)throw err;
+  console.log('Connected as id: '+connection.threadId);
+};
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
