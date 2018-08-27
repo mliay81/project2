978b5dcbfa6e40f58c55f7f879da82da5fb6b986
Abdibasid Osman <mistuyeta@me.com>
2018-08-27 18:30:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var path = require('path');
+var path = requeire('path');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var mysql = require('mysql');
+
 var connection = mysql.createConnection({
   host: 'localhost',
+  port: 3000,
   user: 'root',
   password: 'root',
   database: 'cheers_db',
@@ -16,23 +21,41 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
   if(err)throw err;
   console.log('Connected as id: '+connection.threadId);
-});
+}
 
+app.get('/', function(req, res) {
+  connection.query('SELECT * FROM cheers_db;', function(err, data) {
+    res.render('/');
+  })
+})
 
+// Create a new Coctail
+app.post('/create', function(req, res) {
+  connection.query('INSERT INTO cheers_db VALUES (?)', function(err, result) {
+    if(err)throw err;
+    res.redirect('/')
+  });
+})
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+// Update a Coctails
+app.put('/update', function(req, res) {
+  connection.query("UPDATE cheers_db SET coctail = ? WHERE id = ?", function(err, result) {
+  if(err)throw err;
+  res.redirect('/');
 
-// Routes
-// require("./routes/api.routes")(app);
-// require("./routes/htmlRoutes")(app);
+  });
+})
+
+// Delete a Coctail
+app.delete('/delete', function(req, res) {
+  connection.query("DELETE FROM cheers_db WHERE id = ?", function(err, result) {
+   if(err)throw err;
+   res.redirect('/');
+
+  });
+})
 
 
 app.listen(PORT, function(){
-  console.log('App listening on PORT: ' + PORT);
+  console.log('Server listening on: http://localhost:' + PORT)
 });
-
-module.exports = app;
-module.exports = connection
