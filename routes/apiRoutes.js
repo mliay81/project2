97e191ec74b13520fc8474d07f7cf6cdbcca6ac9
Abdibasid Osman <mistuyeta@me.com>
2018-08-28 15:04:45 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,26 @@
-var db = require("../models");
-
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+app.get('/api', function(req, res) {
+  connection.query('SELECT * FROM cheers_db;', function(err, data) {
+    res.render('/');
+  })
+})
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+// Create a new Coctail
+app.post('/api/create', function(req, res) {
+  connection.query('INSERT INTO cheers_db VALUES (?)', function(err, result) {
+    if(err)throw err;
+    res.redirect('/')
   });
+})
+
+// Delete a Coctail
+app.delete('/delete', function(req, res) {
+  connection.query("DELETE FROM cheers_db WHERE id = ?", function(err, result) {
+   if(err)throw err;
+   res.redirect('/');
+
+  });
+})
+  
 };
