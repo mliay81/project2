ab48665b5451d892ed785c247af2888834ddd5ce
Abdibasid Osman <mistuyeta@me.com>
2018-08-28 17:53:23 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,5 @@
 var db = require('../models/cocktail.js')
 
-module.exports = function (app) {
-  // Get all examples
-  app.get('/api/cocktail', function (req, res) {
-    db.findAll(function (resObj) {
-      console.log(resObj)
-      res.json(resObj)
-    })
-  })
-
-  // Create a new example
-  app.post('/api/cocktail', function (req, res) {
-    db.create(req.body, function (response) {
-      console.log(response)
-      res.redirect('/index')
-    })
-  })
-}
-
 module.exports = function(app) {
 
 app.get('/api', function(req, res) {
