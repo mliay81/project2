e8bf7961de31769a4df87eda4d3f50cb9b419de4
mliay81 <mikeliay@gmail.com>
2018-08-27 22:19:54 -0500

    updates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,45 +1,41 @@
-
-
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-
-      res.render("index", {
-        msg: "Welcome!",
-          bgImage: "/images/cocktail-bg.png"
-
+    // Load index page
+    app.get("/", function(req, res) {
+  
+        res.render("index", {
+          msg: "Welcome!",
+            bgImage: "/images/cocktail-bg.png"
+  
+        });
+  
+    });
+  // loading the search web page
+  
+      app.get("/search", function(req, res) {
+  
+          res.render("search", {
+              msg: "search!",
+              bgImage: "/images/cocktail-bg.png"
+  
+          });
+  
       });
-
-  });
-// loading the search web page
-
-    app.get("/search", function(req, res) {
-
-        res.render("search", {
-            msg: "search!",
-            bgImage: "/images/cocktail-bg.png"
-
-        });
-
+  
+      // loading the create web page
+  
+      app.get("/create", function(req, res) {
+  
+          res.render("create", {
+              msg: "create!",
+              bgImage: "/images/cocktail-bg.png"
+  
+          });
+  
+      });
+  
+  
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+      res.render("404");
     });
-
-    // loading the create web page
-
-    app.get("/create", function(req, res) {
-
-        res.render("create", {
-            msg: "create!",
-            bgImage: "/images/cocktail-bg.png"
-
-        });
-
-    });
-
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
-
-
+  };
